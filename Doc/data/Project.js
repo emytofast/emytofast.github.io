1a71c6dab4fd168e25434ccca93fe26bd7a4782d
Miroiu Emanuel <emytofast@yahoo.com>
2017-05-30 21:50:37 +0300

    Updated documentation

--- Doc/data/Project.js
+++ Doc/data/Project.js
@@ -4,7 +4,7 @@ var sharpDox = sharpDox || {};
 
 sharpDox.projectData = {
   name: 'RuntimeConsole',
-   homepage: 'https://forum.unity3d.com/threads/runtime-console-ugui.467031/',    version: '1.3',    author: 'Miroiu Emanuel',    authorHomepage: 'https://connect.unity.com/u/58caed2932b306001cf7b9c0',     showCode: true,
+   homepage: 'https://forum.unity3d.com/threads/runtime-console-ugui.467031/',    version: '1.3',    author: 'Miroiu Emanuel',    authorHomepage: 'https://connect.unity.com/u/58caed2932b306001cf7b9c0',     showCode: false,
    footerLine: '<p>© 2017 Miroiu Emanuel</p>',   hasLogo: true,
   targetFxs: [
 			".NET Framework 3.5",
