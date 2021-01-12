b14275f7e9a41235a36fc7e6456e09b294690c6b
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-01-13 00:38:58 +0200

    Initial commit

---
+++ Doc/data/Navigation.js
@@ -0,0 +1,309 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'RTC.Base',
+		text: 'RTC.Base',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/RTC.Base' },
+		children: [
+			
+ 
+
+		{
+			id: 'RTC.Base.AbstractAnalyzer',
+			text: 'AbstractAnalyzer',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.AbstractAnalyzer/index' }
+        },	
+		{
+			id: 'RTC.Base.AbstractLogger',
+			text: 'AbstractLogger',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.AbstractLogger/index' }
+        },	
+		{
+			id: 'RTC.Base.AbstractParser',
+			text: 'AbstractParser',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.AbstractParser/index' }
+        },	
+		{
+			id: 'RTC.Base.AnalyzerResult',
+			text: 'AnalyzerResult',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.AnalyzerResult/index' }
+        },	
+		{
+			id: 'RTC.Base.CommandAttribute',
+			text: 'CommandAttribute',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.CommandAttribute/index' }
+        },	
+		{
+			id: 'RTC.Base.CommandEvent',
+			text: 'CommandEvent',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.CommandEvent/index' }
+        },	
+		{
+			id: 'RTC.Base.CommandInfo',
+			text: 'CommandInfo',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.CommandInfo/index' }
+        },	
+		{
+			id: 'RTC.Base.CommandReceivedEvent',
+			text: 'CommandReceivedEvent',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.CommandReceivedEvent/index' }
+        },	
+		{
+			id: 'RTC.Base.ExceptionEvent',
+			text: 'ExceptionEvent',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.ExceptionEvent/index' }
+        },	
+		{
+			id: 'RTC.Base.ExceptionFormatter',
+			text: 'ExceptionFormatter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.ExceptionFormatter/index' }
+        },	
+		{
+			id: 'RTC.Base.Log',
+			text: 'Log',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.Log/index' }
+        },	
+		{
+			id: 'RTC.Base.LoggerEvent',
+			text: 'LoggerEvent',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.LoggerEvent/index' }
+        },	
+		{
+			id: 'RTC.Base.LogTypes',
+			text: 'LogTypes',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.LogTypes/index' }
+        },	
+		{
+			id: 'RTC.Base.ParserResult',
+			text: 'ParserResult',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.ParserResult/index' }
+        },	
+		{
+			id: 'RTC.Base.PlainText',
+			text: 'PlainText',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Base.PlainText/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'RTC.Converter',
+		text: 'RTC.Converter',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/RTC.Converter' },
+		children: [
+			
+ 
+
+		{
+			id: 'RTC.Converter.ArrayConverter',
+			text: 'ArrayConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.ArrayConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.BoolConverter',
+			text: 'BoolConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.BoolConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.ByteConverter',
+			text: 'ByteConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.ByteConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.CharConverter',
+			text: 'CharConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.CharConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.DecimalConverter',
+			text: 'DecimalConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.DecimalConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.DoubleConverter',
+			text: 'DoubleConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.DoubleConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.FloatConverter',
+			text: 'FloatConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.FloatConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.IntConverter',
+			text: 'IntConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.IntConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.ListConverter',
+			text: 'ListConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.ListConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.LongConverter',
+			text: 'LongConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.LongConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.PlainTextConverter',
+			text: 'PlainTextConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.PlainTextConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.SByteConverter',
+			text: 'SByteConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.SByteConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.ShortConverter',
+			text: 'ShortConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.ShortConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.StringConverter',
+			text: 'StringConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.StringConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.UIntConverter',
+			text: 'UIntConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.UIntConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.ULongConverter',
+			text: 'ULongConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.ULongConverter/index' }
+        },	
+		{
+			id: 'RTC.Converter.UShortConverter',
+			text: 'UShortConverter',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Converter.UShortConverter/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'RTC.Core',
+		text: 'RTC.Core',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/RTC.Core' },
+		children: [
+			
+ 
+
+		{
+			id: 'RTC.Core.RTConsole',
+			text: 'RTConsole',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Core.RTConsole/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'RTC.Core.Extension',
+		text: 'RTC.Core.Extension',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/RTC.Core.Extension' },
+		children: [
+			
+ 
+
+		{
+			id: 'RTC.Core.Extension.ConsoleExtensions',
+			text: 'ConsoleExtensions',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Core.Extension.ConsoleExtensions/index' }
+        },	
+		{
+			id: 'RTC.Core.Extension.GenericExtension',
+			text: 'GenericExtension',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#/type/RTC.Core.Extension.GenericExtension/index' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'RTC.Interface',
+		text: 'RTC.Interface',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#/namespace/RTC.Interface' },
+		children: [
+			
+ 
+
+		{
+			id: 'RTC.Interface.IFormatterT',
+			text: 'IFormatter',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#/type/RTC.Interface.IFormatterT/index' }
+        },	
+		{
+			id: 'RTC.Interface.ITypeConverter',
+			text: 'ITypeConverter',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#/type/RTC.Interface.ITypeConverter/index' }
+        },	
+		{
+			id: 'RTC.Interface.ITypeConverterProvider',
+			text: 'ITypeConverterProvider',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#/type/RTC.Interface.ITypeConverterProvider/index' }
+        },	
+		{
+			id: 'RTC.Interface.ITypesProvider',
+			text: 'ITypesProvider',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#/type/RTC.Interface.ITypesProvider/index' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ Doc/data/Project.js
@@ -0,0 +1,12 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.projectData = {
+  name: 'RuntimeConsole',
+   homepage: 'https://forum.unity3d.com/threads/runtime-console-ugui.467031/',    version: '1.3',    author: 'Miroiu Emanuel',    authorHomepage: 'https://connect.unity.com/u/58caed2932b306001cf7b9c0',     showCode: false,
+   footerLine: '<p>© 2017 Miroiu Emanuel</p>',   hasLogo: true,
+  targetFxs: [
+			".NET Framework 3.5",
+	  ]
+}
